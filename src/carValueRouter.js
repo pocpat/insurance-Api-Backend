7470ca4ffa7810fcffc30023b9b74c89c4c329ee
Elena Kroupkin <pocoat@gmail.com>
2023-05-14 00:45:20 +1200

    Post (simple test) works in Postman

--- src/carValueRouter.js
+++ src/carValueRouter.js
@@ -2,14 +2,20 @@ const express = require("express");
 //using Express Router
 const router = express.Router();
 
+//get request test from this route
+router.get("/carValueRouter", (req, res) => {
+  console.log("/carValueRouter");
+  // res.send("Hello Car Value Page");
+  const data = req.body;
+  res.json(data);
+});
 
- 
-  //get request test from this route
-  router.get("/carValueRouter", (req, res) => {
-    console.log("/carValueRouter");
-    res.send("Hello Car Value Page");
-  });
+// POST request for this endpoint
+router.post("/carValueRouter", (req, res) => {
+  console.log("/carValueRouter");
+  const data = req.body;
+  res.json(data);
+});
 
-
-  // exporting route
+// exporting route
 module.exports = router;
