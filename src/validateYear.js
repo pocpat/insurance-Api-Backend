d4bc3ea89a9e6e652c64e8353ca09b061a1f516e
Elena Kroupkin <pocoat@gmail.com>
2023-05-15 00:07:51 +1200

    fix return year in validateYear.js

--- src/validateYear.js
+++ src/validateYear.js
@@ -2,7 +2,7 @@ function validateYear(year) {
     if (/^\d{4}$/.test(year)) {
       const yearNumber = parseInt(year);
       if (yearNumber >= 1900 && yearNumber <= 2023) {
-        return true;
+        return year;
       }
     }
     return false;
