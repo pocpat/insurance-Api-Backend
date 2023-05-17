2600929455fc1560fc30e34c9d9fd10f869f60b3
Jin Zhang <jinzh235@gmail.com>
2023-05-18 00:26:42 +1200

    Typescript start

--- src/validateModel.js
+++
@@ -1,8 +0,0 @@
-function validateModel(model) {
-  if (model === '' || /^[0-9]+$/.test(model)) {
-    return { valid: false, message: 
-      "Model must be a non-empty string containing only letters, numbers, and spaces" };
-  }
-  return model.replace(/[^a-zA-Z]/g, '').toLowerCase();
-}
-module.exports = validateModel;
