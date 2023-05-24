3445efb5294fc56f0349b289ca6f9eff751e05df
Elena Kroupkin <pocoat@gmail.com>
2023-05-24 13:17:21 +1200

    config improvements

--- src/server.ts
+++ src/server.ts
@@ -14,26 +14,3 @@ app.listen(PORT, (): void => console.log(`running on port ${PORT}`));
 
 
 
-
-
-// =============> old code <==================
-// // Middlewares
-// app.use(express.json());
-
-// // Route Imports
-// import carValueRouter from "../routes/carValueRouter";
-// //  import riskRatingRouter from "../routes/riskRatingRouter";
-// // Routes
-// app.use(carValueRouter);
-// //  app.use(riskRatingRouter);
-
-// // =================GET=======================
-// app.get("/", (req, res) => {
-//   res.send("Hello Main Page :)");
-// });
-
-// const PORT = process.env.PORT || 4000;
-// app.listen(PORT, () => {
-//   console.log(`Server Connected! Listening at http://localhost:${PORT}`);
-// });
-// //
--- src/tests/unit/postModelYearController.spec.ts
+++ src/tests/unit/postModelYearController.spec.ts
@@ -1,9 +1,6 @@
 import postModelYearController from "../../../controllers/postModelYearController";
 import { getMockReq, getMockRes } from '@jest-mock/express'
-// import { CarInfo } from "../routes/carValueRouter";
-import { validateCarInfo } from "../../../src/carValServices/validateCarInfo";
 import { carValueData } from "../../carValServices/carValueData";
-import exp from "constants";
 
 describe("postModelYearController", () => {
     test("returns 200 for valid input", () => {
