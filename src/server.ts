a108e4360dd3fe1d726e9e90d0c52d9cd880a140
Elena Kroupkin <pocoat@gmail.com>
2023-05-25 01:37:34 +1200

    add process.env.PORT into server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
 import express from "express";
-// const app = express();
 const router = express.Router();
 
 import app from "./app";
 
-const PORT: Number = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, (): void => console.log(`running on port ${PORT}`));
 
