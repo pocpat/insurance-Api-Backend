fee3b985f1e1579557b6a07944847556e48618d4
Elena Kroupkin <pocoat@gmail.com>
2023-06-16 21:12:09 +1200

    clear all logs

--- src/controllers/getRiskRatingController.ts
+++ src/controllers/getRiskRatingController.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express";
 
 export const getRiskRatingController = (req: Request, res: Response) => {
-  console.log("/riskRatingRouter");
   res.send("Hello Risk Rating Page");
 };
